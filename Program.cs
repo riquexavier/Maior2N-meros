14621a243894caea725e0d634cd30ea1b8d1a882
ETEC Adolpho Berezin <110746209+riquexavier@users.noreply.github.com>
2024-05-03 20:33:33 -0300

    Pedrão do One Two

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int NumberOne;
+int NumberTwo;
+
+Console.Write("Digite um número: ");
+NumberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite outro número: ");
+NumberTwo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+if (NumberOne > NumberTwo)
+{
+    Console.WriteLine($"{NumberOne} é o maior número");
+}
+else
+{
+    Console.WriteLine($"{NumberTwo} é o maior número");
+}
